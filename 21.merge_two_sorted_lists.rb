0125f8e6b86a61f208b1537d1ce263938af68079
surferbv <surferbv@gmail.com>
2022-10-14 12:12:19 -0700

    init 21 merge two sorted lists (easy)

---
+++ 21.merge_two_sorted_lists.rb
@@ -0,0 +1,16 @@
+# 21. Merge Two Sorted Lists
+
+# Definition for singly-linked list.
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val = 0, _next = nil)
+        @val = val
+        @next = _next
+    end
+end
+
+# @param {ListNode} list1
+# @param {ListNode} list2
+# @return {ListNode}
+def merge_two_lists(list1, list2)
+end
