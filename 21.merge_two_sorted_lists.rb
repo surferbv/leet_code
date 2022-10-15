20483d4bdebb5dfc85b240f4c8f7655c31a0ce2b
surferbv <surferbv@gmail.com>
2022-10-14 17:45:36 -0700

    21. Merge Two Sorted Lists

--- 21.merge_two_sorted_lists.rb
+++ 21.merge_two_sorted_lists.rb
@@ -12,5 +12,145 @@ end
 # @param {ListNode} list1
 # @param {ListNode} list2
 # @return {ListNode}
+# recursive approach
+# time: O(m + n) where m + n is the number of stacks calls for the recursive call
+# space: O(m + n) where m is the length of list1 and m is the lenght of list2
 def merge_two_lists(list1, list2)
+    head  = ListNode.new()
+
+    # base case 
+    return list2 if list1 == nil
+    
+    return list1 if list2 == nil
+
+    # recrusive case
+    if list1.val < list2.val 
+        head = list1
+        head.next = merge_two_lists(list1.next, list2)
+    else
+        head = list2
+        head.next = merge_two_lists(list1, list2.next)
+    end
+        
+    return head
 end
+
+# @param {ListNode} list1
+# @param {ListNode} list2
+# @return {ListNode}
+# non recursive approach
+# time: O(m + n) where m + n is the number of stacks calls for the recursive call
+# space: O(1) since were are not adding adding functions onto the call stack
+
+def merge_two_lists(list1, list2)
+    head  = tail = ListNode.new(-1)
+
+    while list1 && list2 
+        if list1.val < list2.val
+            tail.next = list1
+            list1 = list1.next
+        else
+            tail.next = list2
+            list2 = list2.next
+        end
+        tail = tail.next
+    end
+
+    tail.next = list2 if  list1 == nil && list2 != nil
+
+    tail.next = list1 if list2 == nil && list1 != nil
+        
+    return head.next
+end
+
+##################################################################################
+# teaching section
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
