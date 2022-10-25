e0451bb661f971b626a3a5f6ebc500fdafdb7834
surferbv <surferbv@gmail.com>
2022-10-25 13:57:54 -0700

    running sum solutions

--- 1480.running_sum_of_1d_array.rb
+++ 1480.running_sum_of_1d_array.rb
@@ -17,5 +17,38 @@ def running_sum(nums)
     end
 
     return running_sum
+end
+
+# @param {Integer[]} nums
+# @return {Integer[]}
+#
+# iterative approach in place
+# time: O(n) still again we have to iterate over all the elements in the array.
+# space: O(1) we don't allocated a new array since all changes are done in place. 
+# Could have done this with each_with_index as well.                                           
+def running_sum(nums)                       
+    prev_sum = 0                            
     
+    for i in (0..nums.length - 1) do        
+        current_sum = nums[i] + prev_sum
+        nums[i] = current_sum
+        prev_sum = current_sum
+    end
+
+    return nums                             
+end
+
+# @param {Integer[]} nums
+# @return {Integer[]}
+#
+# iterative approach in place and no pre_sum variable
+# time: O(n) still again we have to iterate over all the elements in the array.
+# space: O(1) we don't allocated a new array since all changes are done in place.                                          
+def running_sum(nums)   
+
+    for i in (1..nums.length - 1) do        
+        nums[i] += nums[i - 1]
+    end
+
+    return nums                             
 end
