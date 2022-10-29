f3b1ea676c12b4301525b3782083cd159e0fdc56
surferbv <surferbv@gmail.com>
2022-10-28 17:49:54 -0700

    1672

--- 1672.richest_customer_wealth.rb
+++ 1672.richest_customer_wealth.rb
@@ -1,1 +1,48 @@
 #1672. Richest Customer Wealth
+# @param {Integer[][]} accounts
+# @return {Integer}
+#
+# rubyish way
+# 
+# time: O(m x n) If we have m customers with n accounts we would have to go
+#       m x n of them in the worst case to find the wealthiest customer.
+#
+# space: O(1) we don't allocated any additional data structures. 
+def maximum_wealth(accounts)
+    most_wealth = 0
+
+    accounts.each do |account|
+        wealth = account.reduce(:+)
+        most_wealth = wealth if wealth > most_wealth
+    end
+
+    return most_wealth
+end
+
+# iterative way
+# 
+# time: O(m x n) 
+# space: O(1) 
+def maximum_wealth(accounts)
+    most_wealth = 0
+
+    accounts.each do |account|
+        customer_wealth = 0
+
+        account.each do |balance|
+            customer_wealth += balance
+        end
+
+        most_wealth = wealth if wealth > most_wealth
+    end
+
+    return most_wealth
+end
+
+# one-liner way
+# 
+# time: O(m x n) 
+# space: O(1) 
+def maximum_wealth(accounts)
+    accounts.map {|account| account.sum }.max
+end
