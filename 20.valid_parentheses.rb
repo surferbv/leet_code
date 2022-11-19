e9b89e5c54db2e5f69639ca370d570b426ea1b95
surferbv <surferbv@gmail.com>
2022-11-18 17:16:48 -0800

    blop

--- 20.valid_parentheses.rb
+++ 20.valid_parentheses.rb
@@ -3,7 +3,23 @@
 # https://youtu.be/WTzjTskDFMg
 # @param {String} s
 # @return {Boolean}
+#
+=begin
 
+    hash map approach
+
+    time: O(n)
+        Since we iterate over the list once.
+
+    space: O(n)
+        We store at most n parentheses. 
+
+    note:   
+        Keep in mind that we should only be adding open parentheses first for it to be valid. 
+        If we have any parentheses left on the stack it is invalid.
+
+
+=end
 def is_valid(s)
     return true if s.empty?
 
@@ -26,9 +42,6 @@ def is_valid(s)
     end
     return stack.empty?
 end
-
-
-
 
 =begin
 
