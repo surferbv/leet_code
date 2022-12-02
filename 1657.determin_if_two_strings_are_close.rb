b49b352608f9eea27fb6cf8a1fb1f00e3adbd57f
surferbv <surferbv@gmail.com>
2022-12-01 20:16:25 -0800

    blop

--- 1657.determin_if_two_strings_are_close.rb
+++ 1657.determin_if_two_strings_are_close.rb
@@ -1,7 +1,22 @@
 # 1657. determin if two strings are close
+# https://leetcode.com/problems/determine-if-two-strings-are-close/description/
 # @param {String} word1
 # @param {String} word2
 # @return {Boolean}
+=begin
+
+    hash approach 
+
+    time: O(n)
+        We'd store as at most 26 characters for each hashmap. 
+
+    space: O(1)
+        Max size of each hashmap would be 26 characters. 
+
+    note:
+        You can use hash to ensure the two properties are upheld. Granted we could possible not sort the keys when comparing to improve the time. 
+
+=end
 def close_strings(word1, word2)
     word1_hash, word2_hash = Hash.new(0), Hash.new(0)
 
