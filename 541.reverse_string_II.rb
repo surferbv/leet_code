89d2b37f05448d51d773d471db67e70430fcbccc
surferbv <surferbv@gmail.com>
2022-12-02 20:11:28 -0800

    reverse string

---
+++ 541.reverse_string_II.rb
@@ -0,0 +1,24 @@
+# 541. Reverse String II
+# https://leetcode.com/problems/reverse-string-ii/
+# @param {String} s
+# @param {Integer} k
+# @return {String}
+=begin
+
+    string iteration approach
+
+    time: O(n)
+
+    space: O(n)
+
+    note:
+        Could this by done with two pointers? hmmm... Questions was confusing could not solve this firt time around. 
+
+=end
+def reverse_str(s, k)  
+    0.step(s.length, 2*k) do |i|
+        s[i..(i+k) - 1] = s[i..(i+k) - 1].reverse
+    end
+
+    return s
+end
