d8a179e1197f7a1673f646da3060a209cfeb13d5
surferbv <surferbv@gmail.com>
2023-12-14 19:17:26 -0800

    leeting

--- 283.move_zeros-1.rb
+++ 283.move_zeros-1.rb
@@ -1,5 +1,12 @@
 # @param {Integer[]} nums
 # @return {Void} Do not return anything, modify nums in-place instead.
+# 
+# approach: two pointers
+# time: O(n) as we still need to iterate through all the numbers for each pass
+# space: O(1) since we are not creating any new data structures and only using two pointers 
+# notes:
+# the idea is to think about moving all the non zero values to the front of the array
+# i.e. partion the the array such that all non zeros are on the left and zeros are on the right
 def move_zeroes(nums)
   l = 0 
 
