2178589fb4746ed339cb52422d568a27c8407c57
surferbv <surferbv@gmail.com>
2023-12-25 19:09:35 -0800

    leeting

--- 215.kth_largest_element.rb
+++ 215.kth_largest_element.rb
@@ -20,3 +20,46 @@ def find_kth_largest(nums, k)
   print nums
   nums[k - 1] 
 end
+
+
+# appraoch: quick select
+# time: O(n) on average, O(n^2) worst case
+# space: O(n) 
+# notes: Yeah right, I don't understand this solution at all need to revisist quick select.
+# first get the pivot
+# then partition the array into two parts how? 
+# iterate through the array and swap the elements that are less than the pivot to the left
+# and the elements that are greater than the pivot to the right
+# then swap the pivot with the last element that is less than the pivot
+# then return the index of the pivot
+# Still getting TLE on leetcode
+def find_kth_largest(nums, k)
+  @nums = nums
+  @k = @nums.length - k # kth largest index we are looking for
+
+  def quick_select(l, r)
+    j = rand(l..r) # swap the random index with the last index
+    @nums[j], @nums[r] = @nums[r], @nums[j] # this is to avoid the worst case of O(n^2) when the array is already sorted
+
+    pivot, p = @nums[r], l
+
+    (l...r).each do |i|
+      if @nums[i] < pivot
+        @nums[p], @nums[i] = @nums[i], @nums[p]
+        p += 1
+      end
+    end
+
+    @nums[p], @nums[r] = @nums[r], @nums[p]
+
+    if p > @k
+      return quick_select(l, p - 1)
+    elsif p < @k
+      return quick_select(p + 1, r)
+    else
+      return @nums[p]
+    end
+  end
+
+    return quick_select(0, @nums.length - 1)
+end
